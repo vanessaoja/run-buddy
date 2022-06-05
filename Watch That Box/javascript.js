9cd33b8ddfdc7b9f02961c31c3d2032f266bff5b
Vanessa Oja <vanessaoja@vanessas-air.attlocal.net>
2022-06-04 22:59:59 -0500

    Watch That Box

---
+++ Watch That Box/javascript.js
@@ -0,0 +1,30 @@
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "300px";
+
+}); 
+
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor = "blue";
+
+});
+
+// let box = document.getElementById("box")
+// box.style.opacity = "1";
+// box.style.backgroundColor = 
+// box.style.height = 
+
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity = "0.1";
+
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px";
+    document.getElementById("box").style.backgroundColor = "orange";
+    document.getElementById("box").style.opacity = "1";
+
+});
